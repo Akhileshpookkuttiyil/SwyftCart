8b35efb644eb0fa477e20e4adbd35bef4a819f2a
Akhilesh <akhileshpookkuttiyil@gmail.com>
2026-06-18 21:56:20 +0530

    fix: silence banner icon warning

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -23,15 +23,12 @@ const Banner = () => {
         </p>
         <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600 rounded text-white">
           Buy now
-          <Image
-            className="group-hover:translate-x-1 transition"
-            src={assets.arrow_icon_white}
-            alt="arrow_icon_white"
-            width={11}
-            height={11}
-            style={{ width: "auto", height: "auto" }}
+          <img
+            className="h-[11px] w-[11px] group-hover:translate-x-1 transition"
+            src={assets.arrow_icon_white.src}
+            alt=""
+            aria-hidden="true"
           />
-
         </button>
       </div>
       <Image
